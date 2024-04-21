b52317244f6170ce8b8ed749f1570a238ff6e254
sujaldangal08 <sujaldangal69@gmail.com>
2024-04-21 11:26:35 +0545

    changes to workflow

--- dist/types/app/page.ts
+++ dist/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: F:\Office\sms_landing\src\app\page.tsx
+// File: /home/sujaldangal/Documents/DevOps/sms_landing/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
